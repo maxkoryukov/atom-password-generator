8253e6e34c65e7a8e5b0f0d8e0de6c25e8cb55be
maxkoryukov <maxkoryukov@yandex.ru>
2017-07-08 02:05:56 +0500

    repo: tests refactoring

---
+++ spec/password-generator.test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// const expect = require('chai').expect
+
+describe('AtomPasswordGenerator', function() {
+	let workspaceElement = null
+	let editor = null
+
+	beforeEach(function() {
+
+		return atom.packages.activatePackage('atom-password-generator')
+			.then(pkg => {
+				editor = atom.workspace.buildTextEditor()
+				workspaceElement = atom.views.getView(atom.workspace)
+			})
+	})
+
+	describe('when the password-generator:short event is triggered', function(){
+
+		it('should change text in editor', function() {
+
+			const txt = editor.getText()
+			expect(txt).toBe('')
+			atom.commands.dispatch(workspaceElement, 'password-generator:short')
+			const txt2 = editor.getText()
+
+			expect(txt2).not.toEqual(txt)
+		})
+	})
+})
